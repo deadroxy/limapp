338a7a8030fc8f37d65e6dda612b9ae64a643f2b
Johanna Brewer <deadroxy@frestyl.com>
2013-03-02 11:21:55 +0100

    Basic app framework.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'rake'
+gem 'bundler'
+gem 'rails'
+
+gem 'mysql'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limapp::Application.config.secret_token = '391f97e3c2b09d8c388812fffd7eb1f350e7f910038857d51f9d3e8170b036b09533fc33ac51dccc635c50d3835eda118e1b0a2290458b7dc1ff576ceb90a50c'
